839465f38805a7af50ea66cede39b18b3eb734a9
sujal3797 <115544965+sujal3797@users.noreply.github.com>
2024-10-24 05:28:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //We are using nodejs, not browser
+
+console.log(3
+    +3) //Code readability should be high
+
+console.log("Sujal")
+
+let name = "Sujal"
+let age = 21
+let isLoggedIn = true
+let state
+
+
+// number => 2 to the power of 53
+// bigint
+// string = ""
+// boolean => true/false
+// null = standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
