922359ccd107738c1eb4318c8cdab06fcad43db8
sujal3797 <115544965+sujal3797@users.noreply.github.com>
2024-10-24 09:43:25 +0000

    datatype conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,5 +21,21 @@ console.log(booleanIsLoggedIN)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************************Operations*************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2%3)
+
+// console.log(+true)
+console.log(1 + "2");
+console.log(2+2+"3");
