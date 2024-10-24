e491335ce39d7d2113270fd2b89a695b99eb2334
sujal3797 <115544965+sujal3797@users.noreply.github.com>
2024-10-24 05:41:50 +0000

    datatype conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+// let score = undefined
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "sujal";
+
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIN)
+
+// 1 => true; 0 => false
+// "" => false
+// "sujal" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
