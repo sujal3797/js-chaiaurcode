eb4caaba7151b024dcd3192ba1438012830b4c33
sujal3797 <115544965+sujal3797@users.noreply.github.com>
2024-12-10 19:22:04 +0000

    datatype summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -4,6 +4,8 @@
 // Reference(Non-Primitive)
 // Array, Objects, Functions
 
+// Javascript - Statically or Dynamically typed?
+
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
@@ -21,3 +23,25 @@ const myFunction = function(){
 }
 
 console.log(typeof heros)
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "Sujal"
+let anotherName = myName
+
+anotherName = "Tony"
+
+// console.log(myName);
+// console.log(anotherName);
+
+let user1 = {
+    email: 'hello@email.com',
+    upi: '738838@upi'
+}
+
+let user2 = user1
+
+user2.email = 'bonjour@email.com'
+
+console.log(user1.email)
+console.log(user2.email)
