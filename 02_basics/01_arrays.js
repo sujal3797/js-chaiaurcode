2822de32785ef51c38adcfdaac52fa275e2d77b1
sujal3797 <115544965+sujal3797@users.noreply.github.com>
2024-12-10 20:43:45 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,7 @@
 // Arrays
 
+// Copies - Shallow copy (same reference) & Deep copy
+
 const myArr = [0, 1, 2, 3, 4, 5]
 // console.log(myArr[1]);
 
@@ -9,13 +11,18 @@ const myHeros = ["Shaktiman", "Naagraj"]
 myArr.push(6);
 // console.log(myArr);
 
-myArr.unshift(9);
+myArr.unshift(9); //starting me push karta hai
 // console.log(myArr);
-myArr.shift();
+myArr.shift(); //starting se pop karta hai
 // console.log(myArr.includes(8));
 
 const newArr = myArr.join();
-// console.log(newArr);
+// console.log(newArr); //typeof - string
+
+/* Difference in Slice and Splice
+1. Splice me end range include hoti hai slice me nahi
+2. Splice original array ko hi alter kar deta hai while in case of slice original array remains same
+*/
 
 console.log("A", JSON.stringify(myArr));
 const myn1 = myArr.slice(1,3);
