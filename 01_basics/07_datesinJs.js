21e157b1c4b278ef652edc93af1bc74ae8d15284
sujal3797 <115544965+sujal3797@users.noreply.github.com>
2024-12-10 20:21:31 +0000

    date and time

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,10 +1,10 @@
 // Dates
 
-let myDate = new Date();
-// console.log(myDate.toString());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toISOString());
+let myPrevDate = new Date();
+// console.log(myPrevDate.toString());
+// console.log(myPrevDate.toLocaleDateString());
+// console.log(myPrevDate.toJSON());
+// console.log(myPrevDate.toISOString());
 
 let newDate = new Date(2023, 0, 23, 5, 3);
 // console.log(newDate.toLocaleString());
@@ -17,3 +17,20 @@ let myTimeStamp = Date.now();
 console.log(newDate.toLocaleDateString('default', {
     weekday: "long",
 }));
+
+// Date : Date is an object in javascript. 
+// JavaScript stores dates as number of milliseconds since January 01, 1970
+const myDate = new Date();
+console.log(myDate) //Output : 2023-10-09T14:38:09.847Z
+// We can use various methods to convert this date into more readable value.
+console.log(myDate.toString()) //Output : Mon Oct 09 2023 07:39:18 GMT-0700 (Pacific Daylight Time)
+
+console.log(myDate.toLocaleString()) // Output : 10/9/2023, 7:44:20 AM
+console.log(myDate.toLocaleDateString()) //Output : 10/9/2023
+console.log(myDate.toLocaleTimeString()) // Output : 7:45:31 AM
+
+console.log(myDate.toDateString()) //Output : Mon Oct 09 2023
+console.log(myDate.toTimeString()) // Output : 07:46:40 GMT-0700 (Pacific Daylight Time)
+
+console.log(myDate.toISOString()) // Output : 2023-10-09T14:43:39.337Z
+console.log(myDate.toJSON()) //Output : 2023-10-09T14:40:58.495Z
