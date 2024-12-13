8d3172dcec4947b051b70b6a08d47d86057d8e57
sujal3797 <115544965+sujal3797@users.noreply.github.com>
2024-12-13 17:56:56 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,15 @@
+// for loop
+
+// for (let i=0; i<10; i++) {
+//     console.log(i);
+// }
+
+//break and continue
+
+for(let i=1; i<=20; i++) {
+    if(i == 5) {
+        console.log(`Detected 5`);
+        break;
+    }
+    console.log(i);
+}
